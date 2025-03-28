3f990c7a41fc066bd0d10aa5c65331d938e4e292
giganig <matveykaschuk@gmail.com>
2025-03-28 20:00:20 +0300

    solve task 04

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -6,6 +6,50 @@
 
 #include "logic.h"
 
+int get_max_index(int* array, int size) {
+	int i_max = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (abs(array[i]) > abs(array[i_max])) {
+			i_max = i;
+		}
+	}
+
+	return i_max;
+}
+
+int get_min_index(int* array, int size) {
+	int i_min = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (abs(array[i]) < abs(array[i_min])) {
+			i_min = i;
+		}
+	}
+
+	return i_min;
+}
+
 int sum_between_first_min_max(int* array, int size) {
-	return 0;
+	int sum = 0;
+	
+	int max = get_max_index(array, size);
+	int min = get_min_index(array, size);
+	
+	if (min > max)
+	{
+		int t = max;
+		max = min;
+		min = t;
+	}
+	if (min + 1 == max)
+		return 0;
+	for (int i = min + 1; i < max; i++)
+	{
+		sum += array[i];
+	}
+
+	return sum;
 }
